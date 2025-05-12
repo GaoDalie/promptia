ce72d3cb62244b10ef502dd364f8d5f7d8e493e6
TARIK KAOUTAR <mr.tarik098@gmail.com>
2025-05-11 21:31:38 -0400

    create form page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,53 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import Form from '@components/Form'
 
-const page = () => {
+const CreatePrompt = () => {
+  const { data: session } = useSession(); // Added session destructuring
+  const router = useRouter(); // Fixed Router variable name
+  
+  const [submitting, setSubmitting] = useState(false)
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  })
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      });
+      
+      if(response.ok) {
+        router.push('/'); // Changed Router to router
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
-    <div>CreatePrompt</div>
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
   )
 }
 
-export default page
+export default CreatePrompt
