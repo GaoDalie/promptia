af7b809f9165086dc99ccd384e6ffeb99c82cbff
TARIK KAOUTAR <mr.tarik098@gmail.com>
2025-05-11 22:57:03 -0400

    fix form page

--- components/form.jsx
+++ components/form.jsx
@@ -12,38 +12,42 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
       </p>
       <form
        onSubmit={handleSubmit}
-       className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+       className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphsim"
       >
+        {/* First label with bottom margin */}
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Your AI Prompt</span>
-        
-        <textarea
-           value={post.prompt}
-           onChange={(e) => setPost({...post,
-            prompt: e.target.value
-           })}
-           placeholder="Write your prompt here..."
-           required
-           className="form_textarea"
-           />
-        </label>
-        <label>
-          <span className="font-satoshi font-semibold text-base text-gray-700">
-            Tag
+            Your AI Prompt
           </span>
-        
-        <input
-           value={post.tag}
-           onChange={(e) => setPost({...post,
-            tag: e.target.value
-           })}
-           placeholder="#tag"
-           required
-           className="form_input"
-           />
+          
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({...post,prompt: e.target.value})}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          ></textarea>
         </label>
-        <div className="flex justify-end items-center mx-3 mb-5 gap-4">
+        {/* Second label with margin top to create separation */}
+
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag{" "}
+            <span className="font-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({...post,tag: e.target.value})}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
+        
+        <div className="flex-end mx-3 mb-5 gap-4">
           <Link href="/" className="text-gray-500 text-sm">
             Cancel
           </Link>
@@ -57,7 +61,7 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
         </div>
       </form>
     </section>
-  )
-}
+  );
+};
 
 export default Form
